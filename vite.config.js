feb9deaa0201c9f668ea370c2be05dd8c4e45c28
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2025-01-09 11:18:37 -0500

    update ams
    add packages for ams components
    
    add alias for modules

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './build'
+  },
+  resolve: {
+    alias: [
+      { find: '~', replacement: path.resolve(__dirname, 'src') },
+    ],
+  },
+  define: {
+    'process.env' : {}
+  }
 })
