baa6ba3d23e2509933099df25b239fcaee9f0b08
Alex Muro <am3081@gmail.com>
2025-01-09 11:44:33 -0500

    test 123

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { DmsSite, adminConfig } from "@availabs/dms"
 import Auth from './Auth'
-
+import { withAuth, useAuth } from "./modules/ams/src"
 function App() {
   //console.log('auth', Auth)
   Auth.forEach(d => d.Component = d.element)
@@ -12,6 +12,7 @@ function App() {
             type: 'ams-dev'
           })
         }
+        authWrapper={withAuth}
         routes={[...Auth]} 
       />
   )
